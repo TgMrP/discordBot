fa0da6e50e82f80faf9371bc15c8ab77cf0c3a83
Ran Tayar <tgmrp.dev@gmail.com>
2020-09-12 19:12:19 +0300

    edit info

--- src/commands/info.js
+++ src/commands/info.js
@@ -6,7 +6,7 @@ module.exports = async (msg) => {
   embed.setDescription('Here\'s some info about me');
   embed.addField(':white_check_mark: API Status', `ONLINE & bot latency to this server is ${Math.round(timeTaken)}ms`);
   embed.addField(':spy:  My masters:', 'homeranil[https://github.com/homeranil] & rant1989[https://github.com/rant1989]');
-  embed.addField(':purple_heart:  My Life', 'I\'m currently being hosted on a Heroku');
+  embed.addField(':purple_heart:  My Life', 'I\'m currently being hosted on a Heroku!');
   embed.setColor('#00ff00');
   msg.channel.send(embed);
 };
