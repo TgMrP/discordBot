07ac53f262d7071f801426512eb91e6f022df252
Ran Tayar <tgmrp.dev@gmail.com>
2020-09-12 15:20:43 +0300

    add joke command

--- src/commandHandler.js
+++ src/commandHandler.js
@@ -3,6 +3,7 @@ const sum = require('./commands/sum');
 const eightBall = require('./commands/8ball');
 const kickUser = require('./commands/kick');
 const role = require('./commands/role');
+const joke = require('./commands/role');
 
 const guildID = process.env.GUILD_ID;
 const channelID = process.env.CHANNEL_ID;
@@ -11,6 +12,7 @@ const commands = {
   ping,
   sum,
   role,
+  joke,
   '8ball': eightBall,
   'kick': kickUser
 };
